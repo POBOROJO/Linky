28ba506e321ba61d1ea75904b5c0d09c83a77ad4
POBOROJO <parijatbhatt1@gmail.com>
2024-03-02 00:27:50 +0530

    Updated : index.js file and added get and post routes, Added : connect.js file as the mongoose connection file

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const ShortUrl = require("./models/shortUrl");
 const app = express();
+
+mongoose.connect('mongodb+srv://parijatbhatt1:XriKznROSZhGhSL8@cluster0.2nwhboq.mongodb.net/urlShortener',{
+
+});
 
 app.set("view engine", "ejs");
 
-app.get("/", (req,res) =>{
-    res.render("index")
+app.use(express.urlencoded({extended : false}))
+
+app.get("/", async(req,res) =>{
+    const shortUrls = await ShortUrl.find();
+    res.render("index", {shortUrls: shortUrls});
+})
+
+app.post("/shortUrls",async (req,res) =>{
+    await ShortUrl.create({full : req.body.fullUrl})
+
+    res.redirect("/")
+})
+
+app.get("/:shortUrl", async(req,res) =>{
+    const shortUrl = await ShortUrl.findOne({short : req.params.shortUrl})
+    if(shortUrl == null){
+        return res.status(404)
+    }
+
+    shortUrl.clicks++
+    shortUrl.save()
+    res.redirect(shortUrl.full);
 })
 
 app.listen(process.env.PORT || 4000, () => {
